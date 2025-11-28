261dbac2d83e484881d5ef0ff02f2a7255065f20
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-11-28 23:18:08 +0530

    pyton program

--- scores.py
+++ scores.py
@@ -1,14 +1,15 @@
 import sys
-if len(sys.argv)  > 1:
-    script_name = sys.argv[0]
-    scores = sys.argv[1]
-    scores = [float(x) for x in scores]
+
+if len(sys.argv) > 1:
+    scores = [float(x) for x in sys.argv[1:]] 
 else:
     scores = [50, 60, 70, 80, 90]
     print(f"No input given, using default scores: {scores}")
-    total = sum(scores)
-    average = total/len(scores)
-    print("Scores: ", scores)
-    print("Sum of scores: ", total)
-    print("Average of scores: ", average)
-    
+
+# Always executed:
+total = sum(scores)
+average = total / len(scores)
+
+print("Scores:", scores)
+print("Sum:", total)
+print("Average:", average)
