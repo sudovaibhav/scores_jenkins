5c9b5f91f363ec2d24fbccd06b53f8598c5fad29
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-11-28 23:21:47 +0530

    updated file

--- scores.py
+++ scores.py
@@ -6,10 +6,12 @@ else:
     scores = [50, 60, 70, 80, 90]
     print(f"No input given, using default scores: {scores}")
 
-# Always executed:
 total = sum(scores)
 average = total / len(scores)
 
 print("Scores:", scores)
 print("Sum:", total)
 print("Average:", average)
+min = min(scores)
+max = max(scores)
+print(f"Minimum: {min}\nMaximum: {max}")
